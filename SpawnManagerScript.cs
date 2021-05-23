3aff2421e78ad7386d7f3d49449228fba7a6c8da
Oleksandr435 <74351964+Oleksandr435@users.noreply.github.com>
2021-05-23 18:19:56 +0300

    Add files via upload

---
+++ SpawnManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerScript : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerControlleer PlayerControlleerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        PlayerControlleerScript = GameObject.Find("Player").GetComponent<PlayerControlleer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnObstacle()
+    {
+        if (PlayerControlleerScript.gameOver == false) 
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+        
+    }
+}
